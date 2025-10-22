cab254a52181b4bc0f75e295452f819ba1a5ef89
tech <tech@gmail.com>
2025-10-22 06:25:13 -0500

    env.py alembic

--- alembic/env.py
+++ alembic/env.py
@@ -1,9 +1,14 @@
+import os
+import sys
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+
+# Add the project root to sys.path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
