339fc0efb6401d0cd436df5b7ffe81facc928261
tech <tech@gmail.com>
2025-11-20 16:56:33 -0500

    update cors

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ app = FastAPI(
 if settings.BACKEND_CORS_ORIGINS:
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=[*],
+        allow_origins=["*"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
